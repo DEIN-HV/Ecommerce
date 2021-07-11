d2504beefc42cd78281a2211a938c958ce59046a
DIEN HUYNH VINH <64977759+DEIN-HV@users.noreply.github.com>
2021-07-12 00:03:57 +0700

    add source

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import logo from '../../assets/commerce.png';
+import { classes } from 'istanbul-lib-coverage';
+import useStyles from './style'
+
+
+const Navbar = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <AppBar className={classes.appBar} position="fixed" color="inherit">
+                <Toolbar >
+                    <Typography className={classes.title} variant="h6" color="inherit">
+                        <img src={logo} alt="ecommerce" className={classes.image} height="25px" />
+                        Shoes Store
+                    </Typography>
+
+                    <div className={classes.grow}></div>
+                    <div className={classes.button}>
+                        <IconButton aria-label="show cart items" color="inherit">
+                            <Badge badgeContent="2" color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/components/Product/style.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { findByLabelText } from '@testing-library/react';
+
+export default makeStyles(() => ({
+    root: {
+        maxWidth: '100%',
+    },
+
+    media: {
+        height: '0',
+        paddingTop: '56.25%',
+    },
+
+    cardContent: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+
+    cardActions: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
+}));
