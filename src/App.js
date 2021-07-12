da411d80115d06e0e58bfcd5f20351767e02a4ea
dien.hv <dien.hv@vn.vinx.asia>
2021-07-12 17:42:13 +0700

    update source

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Navbar, Products } from './components';
+import { commerce } from './lib/commerce';
 
 const App = () => {
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    const fetchData = async () => {
+        const { data } = await commerce.products.list();
+        setProduct(data);
+        console.log(data)
+    }
+
+
+
     return (
         <div>
             <Navbar />
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid } from '@material-ui/core';
-import Product from './Product/Product';
+import Product from '../Product/Product';
+import useStyle from './style';
 
 const products = [
     { id: 1, name: 'Men Shoes', description: 'Unique design', price: '100$', image: 'https://i.pinimg.com/originals/eb/8f/48/eb8f48348d1fe3621e95046d95b65f5a.jpg' },
@@ -9,9 +10,11 @@ const products = [
 ]
 
 const Products = () => {
+    const classes = useStyle();
     return (
-        <main>
-            <Grid container justify="center" spacing={4}>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Grid container justifyContent="center" spacing={4}>
                 {products.map(product => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                         <Product product={product} />
