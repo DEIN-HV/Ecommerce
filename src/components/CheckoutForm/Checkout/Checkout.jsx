048620a67ec875817429297f948321c966f826a6
DIEN HUYNH VINH <64977759+DEIN-HV@users.noreply.github.com>
2021-07-18 23:52:44 +0700

    update source

---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import {
+  CssBaseline,
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+} from "@material-ui/core";
+import useStyle from "./style";
+import AddressForm from "../Form/AddressForm";
+import PaymentForm from "../Form/PaymentForm";
+import { useEffect } from "react";
+import { commerce } from "../../../lib/commerce";
+
+const steps = ["Shipping address", "Payment detail"];
+
+function Checkout({ cart }) {
+  const [activeStep, setActiveStep] = useState(0);
+  const classes = useStyle();
+  const [checkoutToken, setCheckoutToken] = useState(null);
+
+  useEffect(() => {
+    const fetchCheckoutToken = async () => {
+      const token = await commerce.checkout.generateToken(cart.id, {
+        type: "cart",
+      });
+      setCheckoutToken(token);
+      console.log("token;", res);
+    };
+
+    fetchCheckoutToken();
+  }, []);
+
+  const Form = () => {
+    return activeStep === 0 ? <AddressForm /> : <PaymentForm />;
+  };
+
+  const Confirmation = () => <div>Confimation</div>;
+
+  return (
+    <>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">
+            Check out
+          </Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
+        </Paper>
+      </main>
+    </>
+  );
+}
+
+export default Checkout;
